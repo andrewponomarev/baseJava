7d4e9eedf5eb15bc31bacd7d1c2f2fbbe0f4b727
andrewponomarev <andrewponomarev@yahoo.com>
2018-03-04 15:12:00 +0300

    Исправлены недочеты в классе ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,48 +2,43 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+
+    private static Integer STORAGE_LENGTH = 10000;
+
+    Resume[] storage = new Resume[STORAGE_LENGTH];
+
     int size = 0;
 
     void clear() {
-        if (size > 0) {
-            storage = new Resume[10000];
-            size = 0;
-        }
+        storage = null;
     }
 
     void save(Resume r) {
-        if (size == storage.length)
-            resize(2*storage.length);
+        if (size == storage.length) {
+            System.out.println("Хранилище заполнено");
+            return;
+        }
         storage[size++] = r;
     }
 
     Resume get(String uuid) {
         int id = findIndex(uuid);
-        if (id < 0) {
-            return null;
-        }
-        return storage[id];
+        return id < 0 ? null : storage[id];
     }
 
     void delete(String uuid) {
         int id = findIndex(uuid);
-        System.arraycopy(storage, id + 1, storage, id, size - id - 1);
-        storage[--size] = null;
-        if (size > 0 && size == storage.length/4) {
-            resize(storage.length/2);
-        }
+        storage[id] = storage[--size];
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        if (size == 0) {
-            return new Resume[1];
-        }
         Resume[] result = new Resume[size];
-        System.arraycopy(storage,0, result, 0, size);
+        if (storage != null) {
+            System.arraycopy(storage,0, result, 0, size);
+        }
         return result;
     }
 
@@ -54,16 +49,12 @@ public class ArrayStorage {
     private int findIndex(String uuid) {
         int id = -1;
         for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].uuid))
+            if (uuid.equals(storage[i].uuid)) {
                 id = i;
+                break;
+            }
         }
         return id;
-    }
-
-    private void resize(int max) {
-        Resume[] temp = new Resume[max];
-        System.arraycopy(storage, 0, temp, 0, storage.length);
-        storage = temp;
     }
 
 }
