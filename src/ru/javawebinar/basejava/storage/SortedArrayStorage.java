fdb96225e5958a6f8d589f2cb9bbb071d3918650
andrewponomarev <andrewponomarev@yahoo.com>
2018-05-11 00:35:26 +0300

    hw6 Comparator added

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -3,13 +3,16 @@ package ru.javawebinar.basejava.storage;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage{
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid);
 
     @Override
     protected Integer getKey(String uuid) {
         Resume searchKey = new Resume(uuid, "dummy");
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 
     @Override
