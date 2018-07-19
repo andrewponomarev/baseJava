111af8ad29167c001bf66dc98b5fe77a3d71aac5
andrewponomarev <andrewponomarev@yahoo.com>
2018-07-19 12:19:46 +0300

    hw11

--- test/ru/javawebinar/basejava/storage/JsonPathStorageTest.java
+++ test/ru/javawebinar/basejava/storage/JsonPathStorageTest.java
@@ -1,7 +1,5 @@
 package ru.javawebinar.basejava.storage;
 
-//import ru.javawebinar.basejava.storage.serializer.JsonStreamSerializer;
-
 import ru.javawebinar.basejava.storage.serializer.JsonStreamSerializer;
 
 public class JsonPathStorageTest extends AbstractStorageTest{
